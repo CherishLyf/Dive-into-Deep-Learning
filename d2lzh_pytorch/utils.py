5dbdfa8a1e60043af99742a7ee42a17653cde210
liuyifan <liuyifan@datafountain.cn>
2021-05-21 17:32:56 +0800

    fix

--- d2lzh_pytorch/utils.py
+++ d2lzh_pytorch/utils.py
@@ -1,5 +1,32 @@
 from IPython import display
 
+import torch
+import torchvision
+import sys
+
 def use_svg_display():
     """Use svg format to display plot in jupyter"""
     display.set_matplotlib_formats('svg')
+
+# 加载 mnist 数据集
+def load_data_fashion_mnist(batch_size, resize=None, root='../datasets/'):
+    """Download the fashion mnist dataset and then load into memory."""
+    trans = []
+    if resize:
+        trans.append(torchvision.transforms.Resize(size=resize))
+    trans.append(torchvision.transforms.ToTensor())
+    
+    transform = torchvision.transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
+    
+    if sys.platform.startswith('win'):
+        num_workers = 0 # 0表示不用额外的进程来加速读取数据
+    else:
+        num_workers = 4
+
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+    return train_iter, test_iter
+    
